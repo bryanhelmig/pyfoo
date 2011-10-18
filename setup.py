6e1a4e91751eb3218612edbaf184642c2badb91b
Bryan Helmig <bryan@bryanhelmig.com>
2011-10-17 20:58:54 -0500

    setuptools compatible

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='pyfoo',
+    version='1.0',
+    description='Pyfoo is a Python wrapper for the Wufoo API.',
+    author='Wufoo',
+    url='https://github.com/wufoo/pyfoo',
+    packages=['pyfoo']
+)
